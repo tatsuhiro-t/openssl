4a21b7466458f6f87a470d5aac895e1c3aaad9f4
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2019-08-13 16:31:56 +0900

    quic: Enable 0RTT

--- ssl/statem/statem_clnt.c
+++ ssl/statem/statem_clnt.c
@@ -450,7 +450,7 @@ static WRITE_TRAN ossl_statem_client13_write_transition(SSL *s)
         return WRITE_TRAN_CONTINUE;
 
     case TLS_ST_PENDING_EARLY_DATA_END:
-        if (s->ext.early_data == SSL_EARLY_DATA_ACCEPTED) {
+        if (!SSL_IS_QUIC(s) && s->ext.early_data == SSL_EARLY_DATA_ACCEPTED) {
             st->hand_state = TLS_ST_CW_END_OF_EARLY_DATA;
             return WRITE_TRAN_CONTINUE;
         }
